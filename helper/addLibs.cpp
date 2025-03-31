e1dfc326ccf9805d2b4a980f92a5045ffbafc05d
Muhammad Haseeb IQBAL <155799767+HaseebIqbal1199@users.noreply.github.com>
2025-03-31 19:53:34 +0500

    Setup template

---
+++ helper/addLibs.cpp
@@ -0,0 +1,19 @@
+// Only Code here to kill bugs
+// Not to satisfy your self by using accessive pre-built methods 
+// .........
+// Syntax:
+//      #include <prebuilt.h>
+//      #include <absolute_path.h>
+//      #include "relative_path.h"
+// .........
+
+// Enjoy your Happy Life 😪
+
+// W8 W8! if you had red all the comments. So Keep one bonus tip in your little mind.
+// u can add namespaces here.
+
+// Header Files Here
+#include <iostream>
+
+// Namespaces Here
+using namespace std;
---
+++ source.cpp
@@ -0,0 +1,9 @@
+// If you are a true coder then don't dare to mess with this file.
+// Write clean and optimized to make the whole proccess seemless.
+
+#include "helper/addLibs.cpp"
+
+// root cause of all problems 😣
+int main() {
+    cout << "Happy Birthday to you 🎈";
+}
