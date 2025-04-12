46670a9c96e3572304e0f526796e6a3ae70c76e8
Muhammad Haseeb IQBAL <155799767+HaseebIqbal1199@users.noreply.github.com>
2025-04-12 16:29:52 +0500

    Delete source.cpp

--- source.cpp
+++
@@ -1,9 +0,0 @@
-// If you are a true coder then don't dare to mess with this file.
-// Write clean and optimized to make the whole proccess seemless.
-
-#include "helper/addLibs.cpp"
-
-// root cause of all problems 😣
-int main() {
-    cout << "Happy Birthday to you 🎈";
-}
